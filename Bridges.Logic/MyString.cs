73ec38ee66d708701535c67d4fc42ec78af4bc92
DiegochaGit <diegochavez287549@correo.itm.edu.co>
2023-03-22 16:16:21 -0500

    Project Finished

---
+++ Bridges.Logic/MyString.cs
@@ -0,0 +1,107 @@
+﻿namespace Bridges.Logic
+{
+    public class MyString
+    {
+        private readonly string _text;
+
+        public MyString(string text)
+        {
+            _text = text;
+        }
+
+        public string ReadBridge()
+        {
+            string myBridge = Console.ReadLine();
+            return myBridge;
+        }
+
+        public bool ValidateBases()
+        {
+            string aux = _text;
+            if (aux[0] != '*' || aux[aux.Length-1] != '*')
+            {
+                return false;
+            }
+            for (int i = 1; i < aux.Length-2; i++)
+            {
+                if (aux[i] == '*')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool ValidateComponents()
+        {
+            string aux = _text;
+            for (int i = 0; i < aux.Length; i++)
+            {
+                if (aux[i] != '*' && aux[i] != '=' && aux[i] != '+')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsSimetric()
+        {
+            string aux = _text;
+            int n = aux.Length / 2;
+            for (int i = 0; i < n; i++)
+            {
+                if (aux[i] != aux[aux.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool ValidatePlatformsAndReinforcements()
+        {
+            string aux = _text;
+            int n = aux.Length / 2;
+            int plataformsCount = 0;
+            for (int i = 1; i < aux.Length-2; i++)
+            {
+                if (aux[i] == '=')
+                {
+                    plataformsCount++;
+                }
+                if (aux[i] == '+' && plataformsCount < 3)
+                {
+                    plataformsCount = 0;
+                }
+                if (aux[i] == '+' && plataformsCount == 3)
+                {
+                    if (i != (n + 2))
+                    {
+                        return false;
+                    } 
+                    else
+                    {
+                        plataformsCount = 0;
+                    }
+                }
+                if (plataformsCount > 3)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public string? PrintString(MyString myBridge)
+        {
+            string output = string.Empty;
+            for (int i = 0; i < _text.Length; i++)
+            {
+                output += $"{_text[i]}";
+            }
+            return output;
+        }
+
+    }
+}
---
+++ Bridges.UI.Console/Program.cs
@@ -0,0 +1,83 @@
+﻿using Bridges.Logic;
+
+var myBridges = new MyString[]
+{
+    new MyString("**"),                   //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*=*"),                  //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*==*"),                 //b=t, c=t, sim=t, pr=t, v=t 
+    new MyString("*===*"),                //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*=+=*"),                //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*=++=*"),               //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*==+==*"),              //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*==++==*"),             //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*==+==+==*"),           //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*==+===+==*"),          //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*==+====+==*"),         //b=t, c=t, sim=t, pr=f, v=f
+    new MyString("*==+===+===+==*"),      //b=t, c=t, sim=t, pr=f, v=f
+    new MyString("*==++==++==*"),         //b=t, c=t, sim=t, pr=t, v=t
+    new MyString("*===+==+===*"),         //b=t, c=t, sim=t, pr=f, v=f
+    new MyString("*==*==*==*"),           //b=f, c=t, sim=t, pr=f, v=f
+    new MyString("*==+==?==*"),           //b=t, c=f, sim=f, pr=f, v=f
+    new MyString("?==+==+==?"),           //b=f, c=f, sim=t, pr=t, v=f
+    new MyString("==+==+=="),             //b=f, c=t, sim=t, pr=t, v=f
+    new MyString("*==+==+=="),            //b=f, c=t, sim=f, pr=t, v=f
+    new MyString("==+==+==*"),            //b=f, c=t, sim=f, pr=t, v=f
+    new MyString("*==*==*"),              //b=f, c=t, sim=t, pr=t, v=f
+    new MyString("*==*==*==*"),           //b=f, c=t, sim=t, pr=f, v=f
+    new MyString("?==+==+===?")           //b=f, c=f, sim=f, pr=t, v=f
+ };
+
+foreach (var myBridge in myBridges)
+{
+    Console.WriteLine("\n___________________________________");
+    Console.Write("\nThe bridge is: ");
+    Console.WriteLine(myBridge.PrintString(myBridge));
+
+    if (myBridge.ValidateBases())
+    {
+        Console.Write("The bridge has valid bases");
+    }
+    else
+    {
+        Console.Write("\nThe bridge hasn't valid bases");
+    }
+
+    if (myBridge.ValidateComponents())
+    {
+        Console.Write("\nThe bridge has valid components");
+    }
+    else
+    {
+        Console.Write("\nThe bridge hasn't valid components");
+    }
+
+    if (myBridge.IsSimetric())
+    {
+        Console.Write("\nThe bridge is simmetrical");
+    }
+    else
+    {
+        Console.Write("\nThe bridge isn't simmetrical");
+    }
+
+    if (myBridge.ValidatePlatformsAndReinforcements())
+    {
+        Console.Write("\nThe bridge has valid platforms and reinforcements");
+    }
+    else
+    {
+        Console.Write("\nThe bridge hasn't valid platforms and reinforcements");
+    }
+
+    if (myBridge.ValidateBases() && myBridge.ValidateComponents() 
+        && myBridge.IsSimetric() && myBridge.ValidatePlatformsAndReinforcements())
+    {
+        Console.Write("\nVALID");
+    }
+    else
+    {
+        Console.Write("\nNONVALID");
+    }
+}
+
+
